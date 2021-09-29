f39fcd02ab768e971a2cb90b30acdf1063528cf7
cristian123-rocketship <cristian123@rocketship.com>
2021-09-29 18:43:33 +0100

    Pick up the latest uikit with a fix for external links in the navigation menu.

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -1,85 +1,84 @@
 export default [
   {
-    label: "Home",
-    href: "https://app.shibanova.io",
+    label: 'Home',
+    href: 'https://app.shibanova.io',
   },
   {
-    label: "NovaDEX",
-    href: "https://swap.shibanova.io",
+    label: 'NovaDEX',
+    href: 'https://swap.shibanova.io',
     button: true,
   },
   {
-    label: "NovaPad",
-    href: "https://app.shibanova.io/novapad",
-  },  
+    label: 'NovaPad',
+    href: 'https://app.shibanova.io/novapad',
+  },
   // {
   //   label: "Audits",
   //   icon: "ShibaNovaAuditIcon",
   //   href: "https://docs.shibanova.io/shibanova-documentation/security/audits",
   // },
   {
-    label: "Earn",
-    icon: "MoreIcon",
+    label: 'Earn',
+    icon: 'MoreIcon',
     items: [
       {
-        label: "Dashboard",
-        href: "https://app.shibanova.io/dashboard",
+        label: 'Dashboard',
+        href: 'https://app.shibanova.io/dashboard',
       },
       {
-        label: "Farms",
-        href: "https://app.shibanova.io/farms",
+        label: 'Farms',
+        href: 'https://app.shibanova.io/farms',
       },
       {
-        label: "Pools",
-        href: "https://app.shibanova.io/pools",
+        label: 'Pools',
+        href: 'https://app.shibanova.io/pools',
       },
       {
-        label: "CrudeOil Vault",        
-        href: "https://app.crudeoil.finance/#/app/vaults",
-        target: "_blank"
+        label: 'CrudeOil Vault',
+        href: 'https://app.crudeoil.finance/#/app/vaults',
+        external: true,
       },
       {
-        label: "AutoShark Vault",        
-        href: "https://autoshark.finance/vaults",
-        target: "_blank"
+        label: 'AutoShark Vault',
+        href: 'https://autoshark.finance/vaults',
+        external: true,
       },
     ],
   },
   {
-    label: "Socials",
-    icon: "MoreIcon",
+    label: 'Socials',
+    icon: 'MoreIcon',
     items: [
       {
-        label: "Telegram",
-        icon: "TelegramIcon",
-        href: "https://t.me/ShibaNovaDEX",
-        target: "_blank"
+        label: 'Telegram',
+        icon: 'TelegramIcon',
+        href: 'https://t.me/ShibaNovaDEX',
+        external: true,
       },
       {
-        label: "Twitter",
-        icon: "TwitterIcon",
-        href: "https://twitter.com/ShibaNovaDefi",
-        target: "_blank"
+        label: 'Twitter',
+        icon: 'TwitterIcon',
+        href: 'https://twitter.com/ShibaNovaDefi',
+        external: true,
       },
       {
-        label: "Medium",
-        icon: "MediumIcon",
-        href: "https://shibanova.medium.com/",
-        target: "_blank"
+        label: 'Medium',
+        icon: 'MediumIcon',
+        href: 'https://shibanova.medium.com/',
+        external: true,
       },
       {
-        label: "Github",
-        icon: "GithubIcon",
-        href: "https://github.com/ShibaNova",
-        target: "_blank"
+        label: 'Github',
+        icon: 'GithubIcon',
+        href: 'https://github.com/ShibaNova',
+        external: true,
       },
       {
-        label: "Docs",
-        icon: "BookIcon",
-        href: "https://docs.shibanova.io",
-        target: "_blank"
+        label: 'Docs',
+        icon: 'BookIcon',
+        href: 'https://docs.shibanova.io',
+        external: true,
       },
     ],
   },
-];
-
+]
